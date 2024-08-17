58ecaa93c5c59744ad1fab05cdc2b7cb509e28a4
Abdullohali <muhammadfoziljonov019@gmail.com>
2024-08-17 20:36:31 +0500

    second

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Project',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
